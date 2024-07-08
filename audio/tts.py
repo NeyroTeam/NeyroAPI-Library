69b34814a48ae12479fade9a1d29734779e02e01
ⱤɆ₱ØⱤ₮ <110277810+rep0rtDev@users.noreply.github.com>
2024-07-08 17:56:22 +0500

    v0.1.7 rework

--- audio/tts.py
+++ audio/tts.py
@@ -1,17 +1,18 @@
-import requests
+import httpx
 import json
-import aiohttp
 
 class AudioAPI:
     @staticmethod
     def tts(api, **kwargs):
         url = f"{api.BASE_URL}/audio/tts"
-        response = requests.post(url, headers=api.headers, data=json.dumps(kwargs))
+        response = httpx.post(url, headers=api.headers, data=json.dumps(kwargs))
+        response.raise_for_status()
         return response.json()
 
     @staticmethod
     async def async_tts(api, **kwargs):
         url = f"{api.BASE_URL}/audio/tts"
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, headers=api.headers, data=json.dumps(kwargs)) as response:
-                return await response.json()
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url, headers=api.headers, data=json.dumps(kwargs))
+            response.raise_for_status()
+            return response.json()

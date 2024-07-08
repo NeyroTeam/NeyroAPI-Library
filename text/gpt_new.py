3ca4b9ced0caa9003bac6d9da0cbd7d1f0f50d9c
ⱤɆ₱ØⱤ₮ <110277810+rep0rtDev@users.noreply.github.com>
2024-07-08 17:55:12 +0500

    v0.1.7 rework

--- text/gpt_new.py
+++ text/gpt_new.py
@@ -1,17 +1,18 @@
-import requests
+import httpx
 import json
-import aiohttp
 
 class TextAPI:
     @staticmethod
     def gpt_new(api, **kwargs):
         url = f"{api.BASE_URL}/text/gpt-new"
-        response = requests.post(url, headers=api.headers, data=json.dumps(kwargs))
+        response = httpx.post(url, headers=api.headers, data=json.dumps(kwargs))
+        response.raise_for_status()
         return response.json()
 
     @staticmethod
     async def async_gpt_new(api, **kwargs):
         url = f"{api.BASE_URL}/text/gpt-new"
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, headers=api.headers, data=json.dumps(kwargs)) as response:
-                return await response.json()
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url, headers=api.headers, data=json.dumps(kwargs))
+            response.raise_for_status()
+            return response.json()
